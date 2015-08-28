5088284b945175008565537e34b078b2c936c222
Lawrence Gowin <lgowin1@comcast.net>
2015-08-27 21:41:28 -0600

    limit financials export

--- app/controllers/financials_controller.rb
+++ app/controllers/financials_controller.rb
@@ -9,7 +9,7 @@ class FinancialsController < ApplicationController
     respond_to do |format|
       format.html
       format.csv { send_data @sessions.to_csv }
-      format.xls { send_data @sessions.to_csv(col_sep: "\t") }
+      format.xls # { send_data @sessions.to_csv(col_sep: "\t") }
     end
   end
 
